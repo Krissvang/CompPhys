611a39a0ba8e4875936e4e8216cfc51e0612727b
Krissvang <Kristian.vangsnes@hotmail.com>
2019-08-31 18:11:03 +0200

    Plots

--- Python_plot/plotting.py
+++ Python_plot/plotting.py
@@ -6,17 +6,23 @@ Created on Sat Aug 31 15:37:43 2019
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(t):
+    return 1-(1-np.exp(-10))*t-np.exp(-10.0*t)
 
 f1=open("E:\\Dokumenter\\compphys\\build-Project1-Desktop_Qt_5_13_0_MinGW_64_bit-Debug\\output1.txt","r")
 f2=open("E:\\Dokumenter\\compphys\\build-Project1-Desktop_Qt_5_13_0_MinGW_64_bit-Debug\\output2.txt","r")
 f3=open("E:\\Dokumenter\\compphys\\build-Project1-Desktop_Qt_5_13_0_MinGW_64_bit-Debug\\output3.txt","r")
 
-flines=f2.readlines()
-xval = []
-solution = []
-exact = []
-error = []
+file=f2
 
+flines=file.readlines()
+xval = np.array([])
+solution = np.array([])
+exact = np.array([])
+error = np.array([])
 
 for x in flines:
     n=0
@@ -26,13 +32,22 @@ for x in flines:
         else:
             j=float(i)
             if n==0:
-                xval.append(j)
+                xval=np.append(xval,j)
             elif n==1:
-                solution.append(j)
+                solution=np.append(solution,j)
             elif n==2:
-                exact.append(j)
+                exact=np.append(exact,j)
             else:
-                error.append(j)
+                error=np.append(error,j)
             n+=1
 
-plt.plot(xval,solution)
+t1=np.arange(xval[0],xval[-1],0.01)
+print(exact[0])
+
+
+plt.plot(xval,solution,'r.',markersize=3)
+plt.title("n=100")
+plt.plot(t1,f(t1),'g')
+plt.xlabel('Position x')
+plt.ylabel('u(x)')
+plt.savefig('n100plot.png')
